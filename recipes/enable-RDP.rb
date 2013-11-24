3533facd79a6863c8d7bc43a4256be8688f2836f
Azul <mail@azulinho.com>
2013-11-24 15:42:31 +0000

    enable RDP

--- recipes/enable-RDP.rb
+++ recipes/enable-RDP.rb
@@ -2,19 +2,23 @@ include_recipe 'windows'
 include_recipe 'powershell'
 
 
-%w(RDS-RD-Server)
+%w(MSRDC-Infrastructure)
    .each do |f|
       windows_feature f do
         action :install
       end
    end
 
-powershell "Set-ItemProperty -Path 'HKLM:\\SYSTEM\\CurrentControlSet\\Control\\Terminal Server' -Name fDenyTSConnections -Value 0'" do
+powershell "Hack the registry" do
+  code <<-EOH
+    Set-ItemProperty -Path 'HKLM:\\SYSTEM\\CurrentControlSet\\Control\\Terminal Server' -Name fDenyTSConnections -Value 0
+  EOH
   action :run
 end
 
-powershell "Set-NetFirewallRule -DisplayGroup 'Remote Desktop' -Enabled True" do
+powershell "Open FW rule" do
+  code <<-EOH
+    Set-NetFirewallRule -DisplayGroup 'Remote Desktop' -Enabled True
+  EOH
   action :run
 end
-
-
