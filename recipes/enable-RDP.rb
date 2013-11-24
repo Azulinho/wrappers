5b2901ad4123dbfbc6fcf613df3787413a0b16bc
Azul <mail@azulinho.com>
2013-11-24 16:24:01 +0000

    using powershell

--- recipes/enable-RDP.rb
+++ recipes/enable-RDP.rb
@@ -1,13 +1,20 @@
 include_recipe 'windows'
 include_recipe 'powershell'
 
-
-%w(Remote-Desktop-Services)
-   .each do |f|
-      windows_feature f do
-        action :install
-      end
-   end
+powershell "install_iis" do
+  flags "#{node.default['rms_IIS']['cmd_flags']}"
+  code <<-EOH
+    Import-Module ServerManager
+    #Check If IIS Services Are Already Installed
+    $check = Get-WindowsFeature | Where-Object {$_.Name -eq "Remote-Desktop-Services"}
+    If ($check.Installed -ne "True") {
+            Add-WindowsFeature Remote-Desktop-Services -IncludeAllSubFeature -Verbose | Out-Null
+    }
+    else {
+            write-output "Remote-Desktop-Services feature already installed"
+    }
+  EOH
+end
 
 powershell "Hack the registry" do
   code <<-EOH
