4d1d42c8aceaf04c0a7d8534cba1514419bd6c3e
Azul <mail@azulinho.com>
2013-11-24 16:30:07 +0000

    add dependency to mysql installer

--- recipes/install-mysql.rb
+++ recipes/install-mysql.rb
@@ -2,21 +2,17 @@
 
 include_recipe 'windows'
 
-#remote_file "#{Chef::Config[:file_cache_path]}/#{node['mysql']['installer_file']}" do
-    #source node['mysql']['url']
-    #checksum "7ca554bcda5d859e71b97f2ed13ec42b"
-#end
-
 windows_package "#{Chef::Config[:file_cache_path]}/#{node['mysql']['installer_file']}" do
   package_name "MySQL Installer"
   source node['mysql']['url']
   checksum node['mysql']['checksum']
   options "/quiet"
   action :install
+  notifies :run , "execute[Install mySQL]"
 end
 
 execute "Install mySQL" do
   command "#{node['mysql']['installer_cli']}"
-  action :run
+  action :nothing
 end
 
