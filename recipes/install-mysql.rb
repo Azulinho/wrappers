73bb2dcb1b6153c92680a8ed19a84b6d3d92c977
Azul <mail@azulinho.com>
2013-11-25 00:24:16 +0000

    use windows_batch

--- recipes/install-mysql.rb
+++ recipes/install-mysql.rb
@@ -2,24 +2,21 @@
 
 include_recipe 'windows'
 
-
-cookbook_file "C:/Windows/temp/install-mysql.bat" do
-  source "install-mysql.bat"
-  action :create
-end
-
 windows_package "#{Chef::Config[:file_cache_path]}/#{node['mysql']['installer_file']}" do
   package_name "MySQL Installer"
   source node['mysql']['url']
   checksum node['mysql']['checksum']
   options "/quiet"
   action :install
-  notifies :run , "execute[Install mySQL]"
+  notifies :run , "windows_batch[Install mySQL]"
 end
 
 
-execute "Install mySQL" do
-  command "C:/Windows/temp/install-mysql.bat"
+windows_batch "Install mySQL" do
+  code <<-EOH
+  cd \\Program\ Files\ (x86)\\mysql\\MySql\ Installer
+MySQLInstallerConsole.exe --config=mysql-server-5.6-winx64:passwd=root --product=mysql-server-5.6-winx64 --catalog=mysql-5.6-winx64 --action=install --type=full --nowait
+  EOH
   action :nothing
 end
 
